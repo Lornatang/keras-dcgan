376dad19a494db3f2092985086dfd7e70e08ab5b
Lornatang <shiyipaisizuo@gmail.com>
2018-11-08 17:40:41 +0800

    modify main.py

--- main.py
+++ main.py
@@ -28,17 +28,17 @@ def generator_model():
     """
     model = Sequential()
     model.add(Dense(input_dim=100, output_dim=1024))
-    model.add(Activation('relu'))
+    model.add(Activation('tanh'))
     model.add(Dense(128 * 7 * 7))
     model.add(BatchNormalization())
-    model.add(Activation('relu'))
+    model.add(Activation('tanh'))
     model.add(Reshape((7, 7, 128), input_shape=(128 * 7 * 7,)))
     model.add(UpSampling2D(size=(2, 2)))
     model.add(Conv2D(64, (5, 5), padding='same'))
-    model.add(Activation('relu'))
+    model.add(Activation('tanh'))
     model.add(UpSampling2D(size=(2, 2)))
     model.add(Conv2D(1, (5, 5), padding='same'))
-    model.add(Activation('relu'))
+    model.add(Activation('tanh'))
     return model
 
 
@@ -48,14 +48,14 @@ def discriminator_model():
         Conv2D(64, (5, 5),
                padding='same',
                input_shape=(28, 28, 1)))
-    model.add(Activation('relu'))
+    model.add(Activation('tanh'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(128, (5, 5)))
-    model.add(Activation('relu'))
+    model.add(Activation('tanh'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(1024))
-    model.add(Activation('relu'))
+    model.add(Activation('tanh'))
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
     return model
@@ -106,7 +106,7 @@ def train(BATCH_SIZE):
             noise = np.random.uniform(-1, 1, size=(BATCH_SIZE, 100))
             image_batch = X_train[index * BATCH_SIZE:(index + 1) * BATCH_SIZE]
             generated_images = g.predict(noise, verbose=0)
-            if index % 400 == 0:
+            if index % 20 == 0:
                 image = combine_images(generated_images)
                 image = image * 127.5 + 127.5
                 Image.fromarray(image.astype(np.uint8)).save(
@@ -120,7 +120,7 @@ def train(BATCH_SIZE):
             g_loss = d_on_g.train_on_batch(noise, [1] * BATCH_SIZE)
             d.trainable = True
             print("batch %d g_loss : %f" % (index, g_loss))
-            if index % 10 == 9:
+            if index % 2 == 0:
                 g.save_weights('generator', True)
                 d.save_weights('discriminator', True)
 
